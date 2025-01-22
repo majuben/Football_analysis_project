c10a80604c637f827412ad41eaa35a1ab8d4a3f0
[majuben] <[charafcherraka@gmail.com]>
2025-01-22 15:34:04 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from utils import read_video, save_video
+from trackers import Tracker 
+from team_assigner import TeamAssigner
+import cv2 #type: ignore
+
+
+def main():
+    video_frames = read_video('input_videos/08fd33_4.mp4')
+    
+    
+    
+    tracker = Tracker('models/best.pt')
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
+    
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    team_assigner = TeamAssigner()  
+    team_assigner.assign_team_color(video_frames[0], tracks["players"][0])
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track["bbox"], player_id)
+            tracks["players"][frame_num][player_id]["team"] = team
+            tracks["players"][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
+    
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    
+    save_video(output_video_frames, 'output_videos/output_video.avi')
+    
+if __name__ == "__main__":
+    main()
